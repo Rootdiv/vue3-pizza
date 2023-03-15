b39f61d4bbab551f6ce31d38e08d99f37cb6b39a
Rootdiv <rootdiv@mail.ru>
2023-03-15 11:35:44 +0300

    feat: changed build config vite

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,21 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: assetInfo => {
+          let extType = assetInfo.name.split('.').pop();
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          } else if (/woff|woff2/i.test(extType)) {
+            extType = 'fonts';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+      },
+    },
+  },
 });
